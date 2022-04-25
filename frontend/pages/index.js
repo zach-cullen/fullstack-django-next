2a0cfbcce347d5039fba6f540dc63f54c5bb35da
Zach Cullen <zacullen@gmail.com>
2022-04-25 16:05:10 -0500

    link to login page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,12 @@
 import PageContainer from "../components/PageContainer"
+import Link from 'next/link'
 
 const Home = () => {
   const title = "Home"
   return (
     <PageContainer title={title}>
       <h1>{title}</h1>
+      <Link href="/login">Log In</Link>
     </PageContainer>
   )
 }
