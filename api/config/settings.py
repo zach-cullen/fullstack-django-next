db0af1de41a1dc0c11a10a4f523b6ba4510f8461
Zach Cullen <zacullen@gmail.com>
2022-05-11 12:29:27 -0500

    Create custom user model

--- api/config/settings.py
+++ api/config/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
